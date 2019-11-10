cf179b889d52d6812cf911ac17d20b71f5238c83
양정민(Austin) <tntnrndh@gmail.com>
2019-11-10 04:11:45 -0500

    -Austin 2019-11-10 Made prototype of Activity 1 and Activity 2. Wrote some of description on Activity 1 and made function that goest to next Activity.

--- app/src/main/java/com/example/zyilin/timecheckapplication/MainActivity.java
+++ app/src/main/java/com/example/zyilin/timecheckapplication/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.zyilin.timecheckapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button acceptButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        Intent i;
+        i = new Intent(MainActivity.this, TimetableActivity.class);
+        startActivity(i);
     }
 }
